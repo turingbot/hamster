5e1855347d1d6f7167b854f190a4a3c8e1043ab0
turingbot <tabaghi.m@gmail.com>
2024-08-12 14:29:25 +0330

    Update ham_keys.sh

--- ham_keys.sh
+++ ham_keys.sh
@@ -211,8 +211,7 @@ main() {
 			else
 				echo "Error generating key for ${games[$game_choice, name]}"
 			fi
-                                telegram_message="--------------------------"
-				send_to_telegram "$telegram_message"
+
 			sleep 10 # wait
 		done
 	done
