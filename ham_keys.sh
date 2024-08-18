1e94f849eca57759d45e01c83f9ada428d10276a
turingbot <tabaghi.m@gmail.com>
2024-08-18 11:55:08 +0330

    Update ham_keys.sh

--- ham_keys.sh
+++ ham_keys.sh
@@ -184,7 +184,7 @@ generate_key_process() {
 		return
 	fi
 
-	for i in {1..20}; do
+	for i in {1..30}; do
 		sleep $((EVENTS_DELAY * (RANDOM % 4 + 1) / 3))
 		has_code=$(emulate_progress "$client_token" "$promo_id" "$proxy")
 
